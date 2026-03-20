95023ac85070016a15350392ccb6f67b88c42441
mayhmemo <mayhm.social@proton.me>
2026-03-19 21:52:24 -0300

    refactor: altera de cpf para documento

--- src/main/java/br/com/nogueiranogueira/aularefatoracao/model/SolicitacaoCredito.java
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/model/SolicitacaoCredito.java
@@ -34,9 +34,10 @@ public class SolicitacaoCredito {
     @Column(nullable = false)
     private Integer score;
 
-    @NotBlank(message = "CPF não pode ser vazio")
-    @Column(nullable = false)
-    private String cpf;
+    @NotBlank(message = "Documento não pode ser vazio")
+    @Pattern(regexp = "\\d{11}|\\d{14}", message = "Documento deve ser CPF (11 dígitos) ou CNPJ (14 dígitos)")
+    @Column(nullable = false, length = 14)
+    private String documento;
 
     @NotNull(message = "Status de negativação não pode ser nulo")
     @Column(nullable = false)
@@ -61,4 +62,3 @@ public class SolicitacaoCredito {
         dataSolicitacao = LocalDateTime.now();
     }
 }
-
